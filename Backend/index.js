eaee0dec65c5cc396bc968cc382ce3b80ccdd4a1
kgakshat8-git <kgakshat8@gmail.com>
2024-06-13 11:24:21 +0530

    EatIndia4

--- Backend/index.js
+++ Backend/index.js
@@ -3,25 +3,25 @@ const app=express()
 const mongoodb=require('./db')
 mongoodb()
 const abc=require("./Routes/Createuser")
-const cors=require('cors')
+//const cors=require('cors')
 //console.log(app);
 
-// app.use((req,res,next)=>{
-//    res.setHeader("Access-Control-Allow-Origin","http://localhost:5173")
-//    res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//    );
-//    next();
-// })
+app.use((req,res,next)=>{
+   res.setHeader("Access-Control-Allow-Origin","eatindia-akshat-kumar-guptas-projects.vercel.app")
+   res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+   );
+   next();
+})
 
-app.use(cors(
-    {
-        origin:["eatindia-akshat-kumar-guptas-projects.vercel.app"],
-        methods:["POST","GET"],
-        credentials:true
-    }
-))
+// app.use(cors(
+//     {
+//         origin:["eatindia-akshat-kumar-guptas-projects.vercel.app"],
+//         methods:["POST","GET"],
+//         credentials:true
+//     }
+// ))
 
 app.get('/funny',(req,res)=>{
     res.send('Hahahaha')
